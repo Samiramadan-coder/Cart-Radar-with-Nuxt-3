b998a9403df233bf5e8c115456e648f9bacd4108
sami <samyramadan.bts@gmail.com>
2023-09-16 22:28:42 +0300

    Finalize project

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    <NavBar />
-    <div
-      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
-    >
-      <CarDetailsHero />
-      <CarDetailsAttribute />
-      <CarDetailsDescription />
-      <CarDetailsContact />
-    </div>
+    <CarDetailsHero />
+    <CarDetailsAttribute />
+    <CarDetailsDescription />
+    <CarDetailsContact />
   </div>
 </template>
+
+<script setup>
+const route = useRoute();
+
+useHead({
+  title: route.params.name
+});
+
+// definePageMeta({
+//   layout: 'custom'
+// });
+</script>
---
+++ pages/city/[city]/car.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="mt-32 flex">
+      <CarSideBar />
+      <NuxtPage />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+
+useHead({
+  title: `Cars In ${route.params.city}`,
+});
+</script>
